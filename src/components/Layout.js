8837fd01a0b59af6dae84fcc2e74d5eba4929214
Yosri Bedoui <yosri.bedoui@pictureworks.com.au>
2021-06-09 19:14:00 +0100

    style: cover picture covers whole page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,9 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => (
-	<div className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style={{ backgroundImage: 'url(\'https://source.unsplash.com/random\')' }}>
+	<div className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style={{
+		minHeight: "100vh",
+		backgroundImage: 'url(\'https://source.unsplash.com/random\')' }}>
 		<Navbar/>
 
 		<main className="bg-cover">{children}</main>
